8ec8b6997102492a5c2089007c43156af6e09a99
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2023-03-29 22:02:04 -0300

    finalizando parte de tratativas e envio de parametros via GET e Post

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,17 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('site.login', ['titulo' => 'login']);
+        $erro = '';
+
+        if($request->get('erro'))
+        {
+            $erro = "Usuário e ou senha inexistentes.";
+        }
+
+        return view('site.login', ['titulo' => 'login', 'erro' => $erro]);
     }
 
-    public function autenticar()
+    public function autenticar(Request $request)
     {
-        return 'Oi';
+        //validações dos campos
+        $regras = array(
+            'usuario' => 'email',
+            'senha' => 'required'
+        );
+
+        $mensagens = array(
+            'usuario.email' => 'Preencha o campo E-mail!',
+            'senha.required' => 'Preencha o campo senha!'
+        );
+
+        $request->validate($regras, $mensagens);
+
+        $user = new User();
+
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        $consultaUsuario = $user->where('email', $email)->where('password', $password)->get()->first();
+
+        if (isset($consultaUsuario->name))
+        {
+            echo 's';
+        }
+        else
+        {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
+
     }
 }
